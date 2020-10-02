05cd121b23f669b2903b32354b7fe62bd4259792
Crowdbotics <team@crowdbotics.com>
2020-10-02 14:56:02 +0000

    Update models

--- backend/quickt_20974/wsgi.py
+++ backend/quickt_20974/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'quickt_20974.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "quickt_20974.settings")
 
 application = get_wsgi_application()
